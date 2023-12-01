a50c8e7ce9b0344ff0bbac8b97e282903517dfe9
PolliShell <polliandfibi@gmail.com>
2023-12-01 11:48:29 +0200

    Add node.js event module

--- full_stack/3.node_js/4.Event_module/index.js
+++ full_stack/3.node_js/4.Event_module/index.js
@@ -3,12 +3,12 @@ const log = require('./log');
 
 const emitter = new EventEmitter();
 
-emitter.on('some_event',(args)=>{
-    const {id, text}=args;
-    console.log(id,text);
+emitter.on('some_event', (args) => {
+    const {id, text} = args;
+    console.log(id, text);
     console.log(text);
 })
 
-emitter.emit('some_event', {id:1,text:'Event test text!'});
+emitter.emit('some_event', {id: 1, text: 'Event test text!'});
 
 log('User Logged');
