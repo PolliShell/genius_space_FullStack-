323fbf3b9f3b47262fea1bcde350ca1539d0d0dd
PolliShell <polliandfibi@gmail.com>
2023-12-01 11:46:39 +0200

    Add node.js buffer and stream

---
+++ full_stack/3.node_js/5.Buffer_and_Stream/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const zlib = require('zlib');
+
+const readStream = fs.createReadStream('./docs/text.txt');
+const writeStream = fs.createWriteStream('./docs/new-text.txt')
+const compressStream = zlib.createGzip();
+
+
+// readStream.on('data',(chunk)=>{
+//     console.log('--------');
+//     console.log(chunk.toString());
+// })
+
+// readStream.on('data',(chunk)=>{
+//    writeStream.write(chunk);
+// })
+
+const handleError = ()=>{
+    console.log('Error');
+    readStream.destroy();
+    writeStream.end('Finish with error...');
+}
+
+readStream
+    .on('error',handleError)
+    .pipe(compressStream)
+    .pipe(writeStream)
+    .on('error',handleError);
