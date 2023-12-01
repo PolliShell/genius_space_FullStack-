17605402f6ad49bdd872f624e2b088349f8ae335
PolliShell <polliandfibi@gmail.com>
2023-12-01 14:04:34 +0200

    Add node.js create server

---
+++ full_stack/3.node_js/6.Create_server/index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const PORT = 3000;
+
+// const server = http.createServer((req, res) => {
+//     console.log('Server request');
+//     console.log(req.url, req.method);
+//
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h1>Hello world!</h1>');
+//     res.end();
+// });
+
+const server = http.createServer((req, res) => {
+    console.log('Server request');
+    console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'application/json');
+
+    const data = JSON.stringify([
+        {name: 'Tommy', age: 35},
+        {name: 'Arthur', age: 40}
+    ])
+
+    res.end(data);
+});
+
+
+server.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
