49b31cebfe2f9cd914a78339cfb4d56de99ed8d3
PolliShell <polliandfibi@gmail.com>
2023-12-01 17:27:45 +0200

    Add node.js npm and express

--- full_stack/2.js/9.Asynchronous/webpack.config.js
+++ full_stack/2.js/9.Asynchronous/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ["@babel/polyfill","./index.js"],
+    entry: ["@babel/polyfill","./server.js"],
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist'),
---
+++ full_stack/3.node_js/9.Express/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+const createPath = (page)=>path.resolve(__dirname,'views',`${page}.html`);
+
+app.listen(PORT,(error)=>{
+    error?console.log(error):console.log(`listening port ${PORT}`);
+});
+
+app.get('/',(req, res)=>{
+    // res.send('<h1>Hello world!</h1>');
+
+    res.sendFile(createPath('index'));
+});
+app.get('/contacts',(req, res)=>{
+    // res.send('<h1>Hello world!</h1>');
+
+    res.sendFile(createPath('contacts'));
+});
+
+
+app.get('/about-us',(req, res)=>{
+    res.redirect('/contacts');
+});
+
+app.use((req,res)=>{
+    res
+        .status(404)
+        .sendFile(createPath('error'))
+});
