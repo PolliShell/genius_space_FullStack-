59241d72b850041682a1c36d58164de587c64b1d
PolliShell <polliandfibi@gmail.com>
2023-12-15 13:35:55 +0200

    Add mvc

--- full_stack/3.node_js/10.View_engine/server.js
+++ full_stack/3.node_js/10.View_engine/server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const Post = require('./models/post');
-const Contact = require('./models/contact');
+const postRoutes = require('./routes/post_routes');
+const contactRoutes = require('./routes/contact-routes')
+const createPath = require('./helpers/create-path');
 
 const app = express();
 
@@ -19,7 +20,6 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
         // Дальнейшие операции с базой данных
     })
     .catch(error => console.error('Ошибка подключения к MongoDB:', error));
-const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
@@ -36,126 +36,9 @@ app.get('/', (req, res) => {
     res.render(createPath('index'), {title});
 });
 
-app.get('/contacts', (req, res) => {
-    const title = 'Contacts';
-    Contact
-        .find()
-        .then((contacts) => res.render(createPath('contacts'), {contacts, title}))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
+app.use(postRoutes);
 
-// app.get('/posts/:id', (req, res) => {
-//     const title = 'Post';
-//     const post = {
-//         id: '1',
-//         text: 'lorem ipsum dolor sit amet consectetur',
-//         title: 'Post title',
-//         date: '05.05.2021',
-//         author: 'Yauhen',
-//     };
-//     res.render(createPath('post'), {title, post});
-// });
-
-app.get('/edit/:id', (req, res) => {
-    const title = 'Edit Post';
-    Post
-        .findById(req.params.id)
-        .then((post) => res.render(createPath('edit-post'), {post, title}))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
-
-app.put('/edit/:id', (req, res) => {
-    const {title, author, text} = req.body;
-    const {id} = req.params;
-    Post
-        .findByIdAndUpdate(id, {title, author, text})
-        .then((result) => res.redirect(`/posts/${id}`))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
-
-app.get('/posts/:id', (req, res) => {
-    const title = 'Post';
-    Post
-        .findById(req.params.id)
-        .then((post) => res.render(createPath('post'), {post, title}))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
-
-app.delete('/posts/:id', (req, res) => {
-    const title = 'Post';
-    Post
-        .findByIdAndDelete(req.params.id)
-        .then(result => {
-            res.sendStatus(200);
-        })
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
-
-
-// app.get('/posts', (req, res) => {
-//     const title = 'Posts';
-//     const posts = [
-//         {
-//             id: '1',
-//             text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
-//             title: 'Post title',
-//             date: '05.05.2021',
-//             author: 'Yauhen',
-//         }
-//     ];
-//     res.render(createPath('posts'), {title, posts});
-// });
-
-app.get('/posts', (req, res) => {
-    const title = 'Posts';
-    Post
-        .find()
-        .sort({createdAt: -1})
-        .then((posts) => res.render(createPath('posts'), {posts, title}))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'});
-        });
-});
-
-app.post('/add-post', (req, res) => {
-    const {title, author, text} = req.body;
-    // const post={
-    //     id: new Date(),
-    //     date: (new Date()).toLocaleDateString(),
-    //     title,
-    //     author,
-    //     text
-    // };
-
-    const post = new Post({title, author, text});
-    post
-        .save()
-        .then((result) => res.send(result))
-        .catch((error) => {
-            console.log(error);
-            res.render(createPath('error'), {title: 'Error'})
-        })
-})
-app.get('/add-post', (req, res) => {
-    const title = 'Add Post';
-    res.render(createPath('add-post'), {title});
-});
+app.use(contactRoutes);
 
 app.use((req, res) => {
     const title = 'Error Page';
